1006031d14fa63d08e5945a8a8b78aa76d1ae61d
Sayon-okai <sayon.study@gmail.com>
2025-07-27 16:02:39 +0000

    Project needs JS logic and few HTML fixes

---
+++ Exam-App/scripts/english.js
@@ -0,0 +1,9 @@
+let examTime = document.querySelector('.time');
+let startScreen = document.querySelector('.start-screen');
+let examScreen = document.querySelector('.exam-screen');
+let examTimer = document.querySelector('.exam-timer');
+let questionNumber = document.querySelector('.question-number');
+let progressBar = document.querySelector('.progress');
+let answers = document.querySelector('.answers');
+
+
